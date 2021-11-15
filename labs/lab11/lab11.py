ddbfb33047fdaff40d6edad62c2b9c5d57effdfc
LexiPadberg <padbergac@g.cofc.edu>
2021-11-15 14:42:34 -0500

    completed lab 11

--- labs/lab11/lab11.py
+++ labs/lab11/lab11.py
@@ -1,10 +1,60 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Lexi Padberg>
+<Lab 11>.py
 """
+from random import randint
+
+
+def words(infile):
+    infile = open(infile, "r")
+    content = infile.read()
+    return content.split("\n")
+
+
+def rand_word(lists):
+    return lists[randint(0, len(lists)-1)]
+
+
+def fill(word, letters):
+    secret = ["_"] * len(word)
+    for letter in letters:
+       for i in range(len(word)):
+        if letter == word[i]:
+            secret[i] = letter
+    return "".join(secret)
+
+
+def won(word, letters):
+    x = fill(word, letters)
+    if x == word:
+        return True
+    else:
+        return False
+
+def play():
+    w = words("wordlist.txt")
+    secret = rand_word(w)
+    current = "_" * len(secret)
+    incorrect = 0
+    guesses = []
+    while incorrect <= 7 and won(secret, guesses) != True:
+        display = fill(secret, guesses)
+        print(display)
+        print(guesses)
+        letter = input(" enter a letter to guess: ")
+        while letter in guesses:
+            letter = input("enter a letter to guess: ")
+        guesses.append(letter)
+        display = fill(secret, guesses)
+        if display != current:
+           incorrect += 1
+        else:
+            current = display
+
+
 
 def main():
-    # add other function calls here
+    play()
     pass
 
 
